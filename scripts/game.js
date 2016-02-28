f62812ee94463e968deb6b1ac6e02275c3a2243d
Jacob Garbe <logodaedalus@gmail.com>
2016-02-28 01:23:23 -0800

    Added UI for players (bot and human) and added pry mechanics

--- scripts/game.js
+++ scripts/game.js
@@ -59,7 +59,7 @@ var Game = Class.extend({
         this.humansPlaying = 0;
         this.numQualities = 3;           //number of qualities used in game (with tarot we have 338 max)
         this.numStartingChips = 25;      //number of starting chips
-        this.autoPlay = false;           //whether play happens automatically
+        this.autoplay = false;           //whether play happens automatically
         
         this.players = [];               //holds players
         this.watchers = [];              //holds who is watching the next action
@@ -89,8 +89,12 @@ var Game = Class.extend({
         }
 
         for (var x=0; x < this.humansPlaying; x++) {
-            var player = new Player("Jacob", false, this);
+            var player = new Player("Jacob", "human", this);        //TODO: this should ask you your name
             this.players.push(player);
+        }
+
+        for (var x=0; x < this.players.length; x++) {   //we can't populate the player model until all the players have been created
+            this.players[x].populatePlayerModel(this);
         }
 
         shuffle(this.players);
@@ -124,9 +128,7 @@ var Game = Class.extend({
 
         for (var x=0; x < this.players.length; x++) {       //for each player
 
-            var panelHtml;
-
-            if (this.players[x].isAI) {     //if it's a bot, create bot display
+            if (this.players[x].type == "bot") {     //if it's a bot, create bot display
                 this.createBotUI(x);
             }
             else {      //if it's human, create human display
@@ -140,15 +142,54 @@ var Game = Class.extend({
     },
 
     createHumanUI: function (playerIndex) {   //creates the interface for human players
-        jQuery('<li/>', {
-                id: 'player' + playerIndex + 'Tab',
-                html: '<a href="#player'+ playerIndex +'Panel">'+ this.players[playerIndex].name + '</a>',
+        
+        jQuery('<li/>', {           //tab
+                id: this.players[playerIndex].name + 'Tab',
+                html: '<a href="#'+ this.players[playerIndex].name +'Panel">'+ this.players[playerIndex].name + '</a>',
             }).appendTo('#UITabs');
 
-        jQuery('<div/>', {
+        jQuery('<div/>', {          //main panel
                 class: 'playerWindow',
-                id: 'player' + playerIndex + 'Panel',
+                id: this.players[playerIndex].name + 'Panel',
             }).appendTo('#panels');
+
+        jQuery('<div/>', {          //current info
+                class: 'info',
+                id: this.players[playerIndex].name + 'Info',
+                html: '<h2>Info</h2>',
+            }).appendTo('#' + this.players[playerIndex].name + 'Panel');
+
+        jQuery('<div/>', {          //make move button
+                class: 'makeMove',
+                id: this.players[playerIndex].name + 'makeMove',
+                html: 'Make move',
+            }).appendTo('#' + this.players[playerIndex].name + 'Panel');
+
+        $( "#" + this.players[playerIndex].name + "makeMove" ).click(function() {
+            this.players[this.currentPlayer].takeTurn(this);
+        });
+
+        jQuery('<div/>', {          //player actions
+                class: 'playerAction',
+                id: this.players[playerIndex].name + 'Action',
+                html: '<h2>Turn History</h2><ul></ul>',
+            }).appendTo('#' + this.players[playerIndex].name + 'Panel');
+
+        $('#' + this.players[playerIndex].name + 'Info').append("<table class='stats' id='"+this.players[playerIndex].name+"Stats'><tr class='tableData'></tr><tr class='tableLabel'></tr></table>");     //add stats table
+
+        var dataHeight = 200;           //height of data columns in pixels
+        var maxQualNum = this.numStartingChips + (this.numStartingChips / 2);           //max out the graph at twice the initial starting chips in one category
+
+        for (var x=0; x < this.numQualities; x++) {
+            $("#" + this.players[playerIndex].name + "Stats .tableData").append("<td class='"+ this.qualities[x].name +"'></td>");
+            var heightPercentage = this.players[playerIndex].secrets[x].value[0] / maxQualNum * 100;
+            var dataCell = this.players[playerIndex].name + "Stats .tableData ." + this.qualities[x].name;
+            $("#" + dataCell).width(100 / this.numQualities + "%");
+            $("#" + dataCell).append("<div>"+ this.players[playerIndex].secrets[x].value[0] +"</div>");
+            $("#" + dataCell + " div").height( heightPercentage + "%");           //set height
+            $("#" + this.players[playerIndex].name + "Stats .tableLabel").append("<td class='"+ this.qualities[x].name +"'>" + this.qualities[x].name + "</td>");
+
+        }
     },
 
     createBotUI: function (playerIndex) {      //creates the interface for bot players
@@ -169,11 +210,37 @@ var Game = Class.extend({
                 html: '<h2>Info</h2>',
             }).appendTo('#' + this.players[playerIndex].name + 'Panel');
 
+        jQuery('<div/>', {          //make move button
+                class: 'makeMove',
+                id: this.players[playerIndex].name + 'makeMove',
+                html: 'Make move',
+            }).appendTo('#' + this.players[playerIndex].name + 'Panel');
+
+        $( "#" + this.players[playerIndex].name + "makeMove" ).click(function() {
+            this.players[this.currentPlayer].takeTurn(this);
+        });
+
         jQuery('<div/>', {          //player actions
                 class: 'playerAction',
                 id: this.players[playerIndex].name + 'Action',
-                html: '<h2>Actions</h2><ul></ul>',
+                html: '<h2>Turn History</h2><ul></ul>',
             }).appendTo('#' + this.players[playerIndex].name + 'Panel');
+
+        $('#' + this.players[playerIndex].name + 'Info').append("<table class='stats' id='"+this.players[playerIndex].name+"Stats'><tr class='tableData'></tr><tr class='tableLabel'></tr></table>");     //add stats table
+
+        var dataHeight = 200;           //height of data columns in pixels
+        var maxQualNum = this.numStartingChips + (this.numStartingChips / 2);           //max out the graph at twice the initial starting chips in one category
+
+        for (var x=0; x < this.numQualities; x++) {
+            $("#" + this.players[playerIndex].name + "Stats .tableData").append("<td class='"+ this.qualities[x].name +"'></td>");
+            var heightPercentage = this.players[playerIndex].secrets[x].value[0] / maxQualNum * 100;
+            var dataCell = this.players[playerIndex].name + "Stats .tableData ." + this.qualities[x].name;
+            $("#" + dataCell).width(100 / this.numQualities + "%");
+            $("#" + dataCell).append("<div>"+ this.players[playerIndex].secrets[x].value[0] +"</div>");
+            $("#" + dataCell + " div").height( heightPercentage + "%");           //set height
+            $("#" + this.players[playerIndex].name + "Stats .tableLabel").append("<td class='"+ this.qualities[x].name +"'>" + this.qualities[x].name + "</td>");
+
+        }
     },
 
     updateUI: function ()  {   //update the UI with values
@@ -185,22 +252,45 @@ var Game = Class.extend({
     },
 
     pryTurn: function(pryerName, targetName, qualityUsed, amountUsed) {
-    //take incoming pry, get defense response from target, adjudicate result, modify world, and return to pryer whether it was successful or not, and what that person's data is
     /*
         -whoever has less in the match, half their quality's chips go to the winner, and they get half their chips back
         -the person that currently has the current game leader of the prying color gets an additional chip
-        -who you pryed is known to everyone, but not the outcome
-    */  
-        //find person with highest amount of prying quality and give them another chip for that quality
-        //get reply: which quality and how much target is putting up
-        //compare qualities and determine winner
-        //if tie, no chips trade hands
-        //whoever wins gets half the other person's chips they put in
+        -TODO: who you pryed is broadcast to everyone, but not the outcome
+    */ 
+        var theAttacker = $.grep(this.players, function (h) { return h.name == pryerName })[0];
+        var attackerWeight = $.grep(this.qualities, function (h) { if (h.name == qualityUsed) { return h; } })[0].weight;
+        var attackerScore = attackerWeight * amountUsed;
+
+        var theTarget = $.grep(this.players, function (h) { return h.name == targetName })[0];
+        var targetResponse = theTarget.parry(this, pryerName, qualityUsed, amountUsed);       //get reply: which quality and how much target is putting up 
+        var targetWeight = $.grep(this.qualities, function (h) { if (h.name == targetResponse.quality) { return h; } })[0].weight;
+        var targetScore = targetWeight * targetResponse.amountUsed;
+
+        var result;         //this is either "won", "lost", or "tie"
+        var chipsNum;
+
+        if (attackerScore > targetScore) {        //if attacker won (winner gets half the other person's chips)
+            result = "won";
+            chipsNum = Math.ceil(targetResponse.amountUsed / 2);        //take half the chips, rounded up
+            $.grep(theAttacker.secrets, function (h) { if (h.quality == targetResponse.quality) { h.value[0]+=chipsNum; h.value[1]+=chipsNum; } });
+            $.grep(theTarget.secrets, function (h) { if (h.quality == targetResponse.quality) { h.value[0]-=chipsNum; h.value[1]-=chipsNum; } });
+        }
+
+        else if (attackerScore < targetScore) {        //if defender won (winner gets half the other person's chips)
+            result = "lost";
+            chipsNum = Math.ceil(amountUsed / 2);        //take half the chips, rounded up
+            $.grep(theAttacker.secrets, function (h) { if (h.quality == qualityUsed) { h.value[0]-=chipsNum; h.value[1]-=chipsNum; } });
+            $.grep(theTarget.secrets, function (h) { if (h.quality == qualityUsed) { h.value[0]+=chipsNum; h.value[1]+=chipsNum; } });
+        }
+
+        else {      //if there was a tie, no chips trade hands
+            result = "tie";
+            chipsNum = 0;
+        }        
         
-        //if won, return reply with additional boolean of whether pry succeeded and info of opponent's response (type and amount)
-        //if lost, return false and that's it
-        //if tie, return "tie" and that's it
-        return "you pried!";
+        this.updateDominion(qualityUsed, 1);        //find person with highest amount of prying quality and give them another chip for that quality
+
+        return ({"result" : result, "amount" : chipsNum});      //return to pryer whether it was successful or not, and the quality delta
     },
 
     waitWatch: function() {
@@ -209,25 +299,44 @@ var Game = Class.extend({
 
     play: function () {       //start a round of the game
 
-        for (this.currentRound; this.currentRound < this.rounds; this.currentRound++) {    //do all rounds of the game
-            console.log("-------------------------------------------");
-            console.log("round " + this.currentRound + " started!");
-            
-            for (this.currentPlayer; this.currentPlayer <= this.numPlayers - 1; this.currentPlayer++) {     //do all turns in the round
-                console.log("player " + this.currentPlayer + " takes a turn!", this.players[this.currentPlayer]);
-                this.players[this.currentPlayer].takeTurn(this);
-            }
-            
-            this.currentPlayer = 0;            //reset to first player for next round
-            
+        this.currentPlayer++;   //increment player
+
+        if (this.currentPlayer == this.numPlayers) {     //if necessary, increment round and reset player
+            this.currentPlayer = 0;
+            this.currentRound++;
         }
 
-        this.decideWinner();                    //after all the turns are taken, let players take their guesses
+        $("#players").tabs("option", "active" , this.currentPlayer);       //click to activate their tab
 
+        if (this.currentRound == this.rounds) {         //if necessary, decide winner
+            this.decideWinner();
+        }
+
+        else if (this.autoplay || this.players[this.currentPlayer].autoplay) {      //otherwise, if autoplay for game is on or autoplay for player is on
+            this.players[this.currentPlayer].takeTurn(this);                        //take turn
+            this.play();                                                            //call play again
+        }
     },
 
     decideWinner: function () {
         console.log("it's over!");
+    },
+
+    updateDominion: function (quality, amount) {
+
+        var qualIndex;
+
+        for (var x=0; x < this.players[0].secrets.length; x++) {
+            if (this.players[0].secrets[x].quality == quality) { qualIndex = x; }
+        }
+
+        var dominion = this.players.reduce(function(prev, current) {
+            return (prev.secrets[qualIndex].value[0] > current.secrets[qualIndex].value[0]) ? prev : current
+        }) //returns object
+
+        dominion.secrets[qualIndex].value[0] += amount;
+        dominion.secrets[qualIndex].value[1] += amount;
+        console.log("the Dominion of " + quality + " (" + dominion.name + ") has gained " + amount);
     }
 });
 
@@ -240,11 +349,5 @@ var Quality = Class.extend({
     },
 });
 
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-
-$(document).ready(function(){
-    $('#pry').click(function(){
-        
-    });
-});
--- scripts/player.js
+++ scripts/player.js
@@ -1,12 +1,15 @@
 var Player = Class.extend({                 //the default player (human-controlled)
-    init: function(theName, isAI, game){
+    init: function(theName, type, game){
         this.name = theName;
-        this.isAI = isAI;
-        this.secrets = [];          //this is the qualities along with their values
-        this.players = [];          //current model of players
+        this.type = type;           //this can be "human", "bot", or "model"
+        this.secrets = [];          //this is the qualities along with their values  { "quality" : "time", "value" : [23,25] }
+        this.players = [];          //current model of players, {"name": "playerName", secrets : [secrets array]
+        this.autoplay = false;
 
-        this.pickPieces(game);
-        this.populatePlayerModel(game);
+        if (type !== "model") {     //if it's not a mental model, initialize it
+            this.pickPieces(game);
+            //this.populatePlayerModel(game);
+        }
     },
     
     populatePlayerModel : function (game) {
@@ -16,19 +19,28 @@ var Player = Class.extend({                 //the default player (human-controll
 
         for (var x in game.players) {
 
+            var playerCopy = new Player(game.players[x].name, false, game);         //create a player for their model of the player
             var secretsInfo = [];
 
             for (var y in game.qualities) {
-                secretsInfo.push([0,game.numStartingChips]);
+                playerCopy.secrets.push({"quality" : game.qualities[y].name, "value" : [0, game.numStartingChips]});
             }
 
-            var startingInfo = { "name" : game.players[x].name, "secrets" : secretsInfo }
-            
-            this.players.push(startingInfo);
+            this.players.push(playerCopy);                //add this to the mental model of the player
         }
     },
-    pickPieces : function (game) {
-
+    pickPieces : function (game) {          //for now, this just assigns random pieces
+        var pieces = game.numStartingChips;
+        for (var x=0; x < game.numQualities - 1; x++) {
+            var value = Math.floor(Math.random()*pieces+0);
+            var secret = {"quality": game.qualities[x].name,"value":[value,value]};
+            this.secrets.push(secret);
+            if (pieces > 0) { pieces -= value;}
+        }
+        if (pieces > 0) {
+            var lastSecret = {"quality": game.qualities[game.numQualities-1].name, "value" : [pieces,pieces]}
+            this.secrets.push(lastSecret);
+        }
     },
     takeTurn : function (game) {        //all players need to have this function
         
@@ -76,25 +88,28 @@ var Player = Class.extend({                 //the default player (human-controll
 //-----------------------------------------------------------------------------
 
 var RandomBot = Player.extend({
-    init: function(theName, isAI, game){
-        this._super(theName, true, game);        
+    init: function(theName, type, game){
+        this._super(theName, "bot", game);       
+        this.autoplay = true; 
     },
 
     pickPieces : function (game) {              //this just picks random amounts for each category
         var pieces = game.numStartingChips;
         for (var x=0; x < game.numQualities - 1; x++) {
             var value = Math.floor(Math.random()*pieces+0);
-            var secret = {"quality": game.qualities[x].name,"value":value};
+            var secret = {"quality": game.qualities[x].name,"value":[value,value]};
             this.secrets.push(secret);
             if (pieces > 0) { pieces -= value;}
         }
         if (pieces > 0) {
-            var lastSecret = {"quality": game.qualities[game.numQualities-1].name, "value" : pieces}
+            var lastSecret = {"quality": game.qualities[game.numQualities-1].name, "value" : [pieces,pieces]}
             this.secrets.push(lastSecret);
         }
     },
 
     takeTurn : function (game) {        //all bots need to have this function
+
+        //not sure what should happen here. Possibly any pre-processing before doing strategy to determine a move?
         this.makeMove(game);
     },
 
@@ -106,30 +121,41 @@ var RandomBot = Player.extend({
         
         var nonZeroQualities = [];                  //grab all your qualities you have chips in
         for (var x in this.secrets) {
-            if (this.secrets[x].value > 0) { nonZeroQualities.push(x); }
+            if (this.secrets[x].value[0] > 0) { nonZeroQualities.push(x); }
         }
 
         if (nonZeroQualities.length == 0) {         //if you're completely out of chips, wait and watch
-            this.waitWatch();
+            this.waitWatch(game);
         }
 
         randomIndex = Math.floor(Math.random()*nonZeroQualities.length+0);          //pick a random quality to use
         var qualityUsed = this.secrets[nonZeroQualities[randomIndex]].quality;
 
-        var amountUsed = Math.floor(Math.random()*this.secrets[nonZeroQualities[randomIndex]].value+1);     //pick a random amount
+        var amountUsed = Math.floor(Math.random()*this.secrets[nonZeroQualities[randomIndex]].value[0]+1);     //pick a random amount
 
         $('#' + this.name + 'Action ul').append("<li>Round "+ game.currentRound + ": " + this.name + " pried " + target + " with " + amountUsed + " " + qualityUsed + "!</li>");
         console.log(this.name + " pried " + target + " with " + amountUsed + " " + qualityUsed + "!");
 
+        console.log(this.name + "'s " + qualityUsed + " = " + this.secrets[nonZeroQualities[randomIndex]].value[0]);
         var outcome = game.pryTurn(this.name, target, qualityUsed, amountUsed);            //take action
+        console.log("it was a " + outcome.result + "! " + this.name + "'s " + qualityUsed + " now = " + this.secrets[nonZeroQualities[randomIndex]].value[0]);
         
         //deal with info from outcome here
     },
 
-    parry: function (game) {
-    //defend, or reply, to an pry. Decide what you will defend with and defend
+    parry: function (game, attacker, quality, amount) {
+    //defend against a pry action. Decide what you will defend with and defend
 
+        var nonZeroQualities = [];                  //grab all your qualities you have chips in
+        for (var x in this.secrets) {
+            if (this.secrets[x].value[0] > 0) { nonZeroQualities.push(x); }
+        }
 
+        randomIndex = Math.floor(Math.random()*nonZeroQualities.length+0);          //pick a random quality to use
+        var qualityUsed = this.secrets[nonZeroQualities[randomIndex]].quality;
+        var amountUsed = Math.floor(Math.random()*this.secrets[nonZeroQualities[randomIndex]].value[0]+1);     //pick a random amount
+
+        return ({"quality" : qualityUsed, "amountUsed" : amountUsed});
         
     },
 
@@ -164,7 +190,6 @@ var RandomBot = Player.extend({
         //this is where strategy should happen, but randomBot just chooses randomly
 
         var actionChoice = Math.floor(Math.random()*3+1);         //pick a number between 1 and 3
-        var target = Math.floor(Math.random()*3+0);
 
         switch (actionChoice) {
             case 1:
